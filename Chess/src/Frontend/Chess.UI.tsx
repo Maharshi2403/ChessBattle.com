34e3e97c248476b48c62a5bd719e8c1613f1d80b
Maharshi Patel <102695974+Maharshi2403@users.noreply.github.com>
2024-08-26 22:12:13 -0400

    Basic UI Componnents added

--- Chess/src/Frontend/Chess.UI.tsx
+++ Chess/src/Frontend/Chess.UI.tsx
@@ -1,32 +1,63 @@
+import React, { useState } from "react";
 import "./App.css";
 
 function ChessApp() {
+  // Toggle menu visibility
+
   return (
     <>
-      <div className="nav">
-        <div className="nav-left">
+      <MenuBar />
+    </>
+  );
+}
+
+function MenuBar() {
+  const [menuDisplayed, setMenuDisplat] = useState(false);
+
+  function setSidBar() {
+    setMenuDisplat(!menuDisplayed);
+
+    if (menuDisplayed == true) {
+      const sidebar = document.querySelector(".sideBar");
+      if (sidebar && sidebar instanceof HTMLElement) {
+        sidebar.style.display = "block";
+      }
+    } else {
+      const sidebar = document.querySelector(".sideBar");
+
+      if (sidebar && sidebar instanceof HTMLElement) {
+        sidebar.style.display = "none";
+      }
+    }
+  }
+  return (
+    <>
+      <div className="topNav">
+        <div className="menuBox">
+          <button className="menuBtn" onClick={setSidBar}>
+            ☰
+          </button>
+        </div>
+        <div className="logoBox">
           <span className="logo">ChessBattle.com</span>
-          <span className="navleft">
-            <button className="menuButton">Resigne</button>
-          </span>
-          <span className="navleft">
-            <button className="menuButton">Draw</button>
-          </span>
-          <span className="navleft">
-            <button className="menuButton">Pause</button>
-          </span>
         </div>
       </div>
-
-      <div className="chessBoard">
-        <div className="nav-right">
-          <div className="mac-buttons">
-            <div className="mac-button close"></div>
-            <div className="mac-button minimize"></div>
-            <div className="mac-button maximize"></div>
-          </div>
+      <div className="sideBar">
+        <div className="btnBox">
+          <button>Resign</button>
         </div>
-        <div className="Board"></div>
+        <div className="btnBox">
+          <button>Draw</button>
+        </div>
+        <div className="btnBox">
+          <button>Sign-up</button>
+        </div>
+        <div className="btnBox">
+          <button>Sign-in</button>
+        </div>
+        <div className="btnBox">
+          <button>Computer</button>
+        </div>
       </div>
     </>
   );
